215dfde5179ccc077faf4a2d4163ead6f062cd2a
Matheus-Assumpcao <99406165+Matheus-Assumpcao@users.noreply.github.com>
2024-03-05 14:45:04 -0300

    add

---
+++ 3_tipos_de_dados/2_exercicio_3/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    $numero1 = 1;
+    $numero2 = 9;
+    $numero3 = 5;
+
+    echo $numero1;
+    echo "<br>";
+    echo $numero2;
+    echo "<br>";
+    echo $numero3;
+    echo "<br>";
+
+    $conta1 = $numero1 + $numero2;
+    echo $conta1;
+
+    echo "<br>";
+
+    $conta2 = $numero3 - $numero2;
+    echo $conta2;
+
+?>
