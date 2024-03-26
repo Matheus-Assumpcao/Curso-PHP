ea84ea961e0e6532fef8b5256b87f5f15a5359d0
Matheus-Assumpcao <99406165+Matheus-Assumpcao@users.noreply.github.com>
2024-03-26 16:57:32 -0300

    add

---
+++ 6_estrutura_de_controle/11_exercicio_26/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $velocidade_carro = 50;
+    $velocidade_permitida = 40;
+
+    if($velocidade_carro < $velocidade_permitida) {
+
+        echo "Velocidade dentro do permitido";
+
+    } elseif($velocidade_carro === $velocidade_permitida) {
+
+        echo "Cuidado! Velocidade permitida é 40 km/h";
+
+    } elseif($velocidade_carro > $velocidade_permitida) {
+
+        echo "ACIMA DA VELOCIDADE PERMITIDA, TOMOU MULTA!";
+    }
+
+?>
