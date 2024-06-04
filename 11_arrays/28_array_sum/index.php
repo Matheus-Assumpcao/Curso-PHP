5f20702d10f5d302839820ac39a9288916cc956a
Matheus Assumpção <matheusassumpcap@gmail.com>
2024-06-04 14:56:50 -0300

    add

---
+++ 11_arrays/28_array_sum/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $arr = [2, 4 ,8, 32.5, 20];
+
+    $soma = array_sum($arr);
+
+    print_r($arr);
+    echo "<br>";
+    echo $soma;
+
+?>
---
+++ 11_arrays/29_array_merge/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $arr1 = [1, 2, 3];
+    $arr2 = [4, 5, 6];
+    $arr3 = [7, 8, 9];
+    $arr4 = ["Dez", "Onze", "Doze"];
+
+    $arrMerge = array_merge($arr1, $arr2, $arr3, $arr4);
+
+    print_r($arrMerge);
+    echo "<br>";
+
+?>
